a444f535d0a487601d7a6ac2c01dd5d86b38fbba
syphu1993 <phusylee@gmail.com>
2021-12-21 21:11:48 +0700

    first commit woza

---
+++ WOZA/resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>WOZA</title>
+</head>
+
+<body>
+    <h1 style="text-align: center;">WOZA</h1>
+</body>
+
+</html>
